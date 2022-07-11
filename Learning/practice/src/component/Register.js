1f0ea94bd9953e5aea3766adccf16bd0ecf823e4
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2022-07-11 18:31:15 +0530

    update

--- Learning/practice/src/component/Register.js
+++ Learning/practice/src/component/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Segment, Form } from 'semantic-ui-react'
 import { useDispatch, useSelector } from "react-redux"
 
@@ -12,12 +12,39 @@ function Register() {
     useSelector((state) => state.menu.activemenu)
 
     const beforeLogin = (e) => {
+        e.preventDefault();
         dispatch({
             type: "ACTIVE_MENU",
             path: '/login'
         })
         navigate("/login")
     }
+
+    const user = {}
+    
+    let getFirstName = (e) => {
+        let val = e.target.value;
+        user.firstname = val;
+    }
+    let getLastName = (e) => {
+        let val = e.target.value;
+        user.lastname = val;
+    }
+    let getEmail = (e) => {
+        let val = e.target.value;
+        user.email = val;
+    }
+    let getPassword = (e) => {        
+        let val = e.target.value;
+        user.password = val;
+    }
+
+    let registerUser = (e) => {
+        console.warn(user);
+    }
+    
+
+
 
     return (<>
         <div>
@@ -34,21 +61,21 @@ function Register() {
                     <Form.Group widths='equal'>
                         <Form.Field>
                             <label>First Name</label>
-                            <input placeholder='Enter Email' name="email" />
+                            <input placeholder='Enter Email' name="firstname" onChange={getLastName} />
                         </Form.Field>
                         <Form.Field>
                             <label>Last Name</label>
-                            <input placeholder='Enter Email' name="email" />
+                            <input placeholder='Enter Email' name="lastname" onChange={getFirstName} />
                         </Form.Field>
                     </Form.Group>
 
                     <Form.Field>
                         <label>Email</label>
-                        <input placeholder='Enter Email' name="email" />
+                        <input placeholder='Enter Email' name="email" onChange={getEmail} />
                     </Form.Field>
                     <Form.Field>
                         <label>Password</label>
-                        <input placeholder='Enter Password' name="password" />
+                        <input placeholder='Enter Password' type="password" name="password" onChange={getPassword}/>
                     </Form.Field>
 
                     <div style={{ marginBottom: "10px", fontWeight: "bold" }}>
@@ -56,7 +83,7 @@ function Register() {
                         <br />
                     </div>
 
-                    <Button type='submit' color="red">Submit</Button>
+                    <Button type='button' color="red" onClick={registerUser} >Submit</Button>
                 </Form>
             </Segment>
         </div>
